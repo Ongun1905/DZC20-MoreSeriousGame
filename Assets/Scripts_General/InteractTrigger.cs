705983326ef1ecb0667f57b4a1d8081fcae2af12
Ongun1905 <53625744+Ongun1905@users.noreply.github.com>
2022-01-25 11:48:24 +0100

    Added physicists and main menu

--- Assets/Scripts_General/InteractTrigger.cs
+++ Assets/Scripts_General/InteractTrigger.cs
@@ -2,21 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class InteractTrigger : MonoBehaviour
 {
-    public Image einsteinCutscene;
+    public Image cutscene;
     public BoxCollider portalTrigger;
     public GameObject portal;
+    public TextMeshProUGUI taskPrompt;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
+            //Einstein
             Debug.Log("cutscene appear");
-            einsteinCutscene.enabled = true;
+            cutscene.enabled = true;
+
+            //Portal
             portalTrigger.enabled = false;
             portal.SetActive(true);
+
+            //TaskPrompt
+            taskPrompt.text = "Jump into the portal!";
+
         }
     }
 
@@ -24,7 +33,7 @@ public class InteractTrigger : MonoBehaviour
     {
         if(collision.CompareTag("Player"))
         {
-            einsteinCutscene.enabled = false;
+            cutscene.enabled = false;
         }
     }
 }
